dde70e1dded1925ee8445e8fa644c4302ddb8207
Arpa Barua <arpa@paddl.co>
2022-10-07 01:44:49 +1100

    Basic file upload functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,50 @@
 import "./App.css";
+import { useState, useEffect } from "react";
+import { storage } from "./firebase";
+import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
+import { v4 } from "uuid";
 
 function App() {
-  return <div className="App"></div>;
+  const [fileUpload, setFileUpload] = useState(null);
+  const [fileList, setFileList] = useState([]);
+
+  const fileListRef = ref(storage, "files/");
+
+  const uploadFile = () => {
+    if (fileUpload === null) return;
+
+    const fileRef = ref(storage, `files/${fileUpload.name + v4()}`);
+    uploadBytes(fileRef, fileUpload).then((snapshot) => {
+      getDownloadURL(snapshot.ref).then((url) => {
+        setFileList((prev) => [...prev, url]);
+      });
+    });
+  };
+
+  useEffect(() => {
+    listAll(fileListRef).then((response) => {
+      response.items.forEach((item) => {
+        getDownloadURL(item).then((url) => {
+          setFileList((prev) => [...prev, url]);
+        });
+      });
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <input
+        type="file"
+        onChange={(event) => {
+          setFileUpload(event.target.files[0]);
+        }}
+      />
+      <button onClick={uploadFile}>Upload File</button>
+      {fileList.map((url) => {
+        return <object data={url} width="400" height="300"></object>;
+      })}
+    </div>
+  );
 }
 
 export default App;
