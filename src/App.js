272f432927332886f30ab32fe86aac6b63258a6c
Arpa Barua <arpa@paddl.co>
2022-10-10 21:01:21 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { db, storage } from "./firebase";
 import { ref, uploadBytes } from "firebase/storage";
 import { v4 } from "uuid";
 import { collection, addDoc } from "firebase/firestore";
 import { Form } from "./components/Form/Form.jsx";
+import { PrintSummary } from "./components/PrintSummary/PrintSummary.jsx";
 
 function App() {
-  const [printJobs, setPrintJobs] = useState([]);
   const [fileUpload, setFileUpload] = useState(null);
   const [title, setTitle] = useState("");
   const [copyNumber, setCopyNumber] = useState(0);
@@ -37,16 +38,6 @@ function App() {
     });
   };
 
-  useEffect(() => {
-    const getPrintJobs = async () => {
-      const data = await getDocs(printJobsCollectionRef);
-      setPrintJobs(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
-    };
-
-    getPrintJobs();
-  }, []);
-
-
   return (
     <div className="App">
       <Form
@@ -56,33 +47,15 @@ function App() {
         onColourChange={(e) => setColour(e.target.value)}
         onSideChange={(e) => setSide(e.target.value)}
         onSubmit={onSubmit}
+        onFileChange={(e) => setFileUpload(e.target.files[0])}
+        onFileUpload={uploadFile}
         paperSize={paperSize}
         colour={colour}
         side={side}
         disabled={title === "" || copyNumber === 0}
       />
-      {printJobs
-        .slice(-10)
-        .map((printJob) => {
-          return (
-            <div>
-              <p>Title: {printJob.title}</p>
-              <p>Number of copies: {printJob.copyNumber}</p>
-              <p>Paper size: {printJob.paperSize}</p>
-              <p>Colour: {printJob.colour}</p>
-              <p>Side: {printJob.side}</p>
-              <p>Status: {printJob.status}</p>
-            </div>
-          );
-        })
-        .reverse()}
-      <input
-        type="file"
-        onChange={(event) => {
-          setFileUpload(event.target.files[0]);
-        }}
-      />
-      <button onClick={uploadFile}>Upload File</button>
+
+      <PrintSummary />
     </div>
   );
 }
